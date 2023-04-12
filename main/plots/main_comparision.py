e74280158441049bfffbcb7c86113b08d1f7d0cb
Jeff Hykin <jeff.hykin@gmail.com>
2023-04-12 11:27:22 -0500

    with all data

--- main/plots/main_comparision.py
+++ main/plots/main_comparision.py
@@ -1,6 +1,7 @@
 import ez_yaml
 from blissful_basics import FS
 import pandas as pd
+import os
 
 from config import config, path_to
 from specific_tools.data_gathering import get_recorder_data
@@ -25,10 +26,26 @@ for name, data in get_recorder_data("@NO_ADJUSTER", "@NORMAL_ADJUSTER", "@PERFEC
         )
     )
 
+should_flatten_graph = True
+if should_flatten_graph:
+    # find the min y value for each x
+    from collections import defaultdict
+    per_x_value = defaultdict(lambda:[])
+    for each_line in lines:
+        for each_x, each_y in zip(each_line["x_values"], each_line["y_values"]):
+            per_x_value[each_x].append(each_y)
+    min_per_x = {}
+    for each_x, values in per_x_value.items():
+        min_per_x[each_x] = min(values)
+    # flatten all the data
+    for each_line in lines:
+        for index, (each_x, each_y) in enumerate(zip(each_line["x_values"], each_line["y_values"])):
+            each_line["y_values"][index] = each_y - min_per_x[each_x]
+
 graph_lines(
     *lines,
     title="Heavy Noise Comparision",
     x_axis_name="Timestep",
     y_axis_name="AccumulatedReward",
-    save_to=FS.local_path(FS.name(__file__)+".html"),
+    save_to="./main/plots/"+FS.name(__file__)+".html",
 )
