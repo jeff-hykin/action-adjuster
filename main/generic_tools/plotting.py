ff629d4876594dd6e25ae81d8831a20f3a070ea5
Jeff Hykin <jeff.hykin@gmail.com>
2023-04-12 09:48:26 -0500

    improve plotting

--- main/generic_tools/plotting.py
+++ main/generic_tools/plotting.py
@@ -1,3 +1,52 @@
+from blissful_basics import wrap_around_get, stringify
+
+class Colors:
+    def __init__(self, color_mapping):
+        self._color_mapping = color_mapping
+        for each_key, each_value in color_mapping.items():
+            if isinstance(each_key, str) and len(each_key) > 0 and each_key[0] != '_':
+                setattr(self, each_key, each_value)
+    
+    def __getitem__(self, key):
+        if isinstance(key, int):
+            return wrap_around_get(key, list(self._color_mapping.values()))
+        elif isinstance(key, str):
+            return self._color_mapping.get(key, None)
+    
+    def __repr__(self):
+        return stringify(self._color_mapping)
+    
+    def __iter__(self):
+        for each in self._color_mapping.values():
+            yield each
+
+xd_theme = Colors({
+    "black":            '#000000',
+    "white":            '#ffffff',
+    "light_gray":       '#c7cbcd',
+    "cement":           '#698098',
+    "gray":             '#546e7a',
+    "brown":            '#ce9178',
+    "rust":             '#c17e70',
+    "orange":           '#f78c6c',
+    "yellow":           '#fec355',
+    "bananna_yellow":   '#ddd790',
+    "lime":             '#c3e88d',
+    "green":            '#4ec9b0',
+    "bold_green":       '#4ec9b0d0',
+    "vibrant_green":    '#04d895',
+    "dim_green":        '#80cbc4',
+    "light_slate":      '#64bac5',
+    "dark_slate":       '#3f848d',
+    "light_blue":       '#89ddff',
+    "blue":             '#82aaff',
+    "electric_blue":    '#00aeffe7',
+    "purple":           '#c792ea',
+    "pink":             '#e57eb3',
+    "red":              '#ff5572',
+    "soft_red":         '#f07178',
+})
+
 def graph_lines(*args, title, x_axis_name, y_axis_name, ):
     """
         Example:
@@ -5,7 +54,8 @@ def graph_lines(*args, title, x_axis_name, y_axis_name, ):
                 dict(
                     x_values=[0,1,2,3],
                     y_values=[0,1,1,2],
-                    name="line 1"
+                    name="line 1",
+                    color="", # optional
                 ),
                 dict(
                     x_values=[0,1,2,3],
@@ -33,4 +83,7 @@ def graph_lines(*args, title, x_axis_name, y_axis_name, ):
     }
     df = pd.DataFrame(data)
     fig = px.line(df, x=x_axis_name, y=y_axis_name, color="Name", title=title)
+    for line_index, line_info in enumerate(args):
+        if line_info.get("color", None):
+            fig.data[line_index].line.color = line_info["color"]
     fig.show()
--- main/plots/main_comparision.py
+++ main/plots/main_comparision.py
@@ -4,24 +4,30 @@ import pandas as pd
 
 from config import config, path_to
 from specific_tools.data_gathering import get_recorder_data
-from generic_tools.plotting import graph_lines
+from generic_tools.plotting import graph_lines, xd_theme
 
 lines = []
-for name, data in get_recorder_data("@NO_ADJUSTER|0.ignore", "@NORMAL_ADJUSTER|0.ignore", "@PERFECT_ADJUSTER|0.ignore",):
+for name, data in get_recorder_data("@NO_ADJUSTER", "@NORMAL_ADJUSTER", "@PERFECT_ADJUSTER",):
     print(f'''processing {name}''')
     # data.records[0] = {"accumulated_reward": 0, "reward": 0, "timestep": 700, "line_fit_score": -0.18230862363710315}
+    group_name = name.replace("@","").split("|")[0]
     reward_data = [ each for each in data.records if each.get("accumulated_reward", None) != None ]
     lines.append(
         dict(
             x_values=[ each.timestep           for each in reward_data ],
             y_values=[ each.accumulated_reward for each in reward_data ],
             name=name.replace("@","").replace("|"," ").lower(),
+            color=dict(
+                NO_ADJUSTER=xd_theme.red,
+                NORMAL_ADJUSTER=xd_theme.blue,
+                PERFECT_ADJUSTER=xd_theme.green,
+            )[group_name],
         )
     )
 
 graph_lines(
     *lines,
-    title="Line Fit Score",
+    title="Heavy Noise Comparision",
     x_axis_name="Timestep",
     y_axis_name="AccumulatedReward",
 )
