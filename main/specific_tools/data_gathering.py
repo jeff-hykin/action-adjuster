764c90ca8bb60bae4dec3abcc8a7a045def198fc
Jeff Hykin <jeff.hykin@gmail.com>
2023-04-17 16:17:33 -0500

    make loading data more efficient

--- main/specific_tools/data_gathering.py
+++ main/specific_tools/data_gathering.py
@@ -4,6 +4,28 @@ import pandas as pd
 
 from config import config, path_to
 
+recursive_lazy_dict = lambda arg: arg if not isinstance(arg, dict) else LazyDict({ key: recursive_lazy_dict(value) for key, value in arg.items() })
+
+path_cache = {}
+def load_recorder(path):
+    path = FS.make_absolute_path(path)
+    if not path_cache.get(path, None):
+        output = None
+        if FS.is_file(path):
+            data = recursive_lazy_dict(
+                ez_yaml.to_object(
+                    string=FS.read(path)
+                )
+            )
+            try:
+                data.config = data.parent_data_snapshot.config
+            except Exception as error:
+                pass
+            data.records = [ LazyDict(each) for each in data.records ]
+            output = data
+        path_cache[path] = data
+    return path_cache[path]
+
 def get_recorder_data(*names, quiet=False):
     which_experiments = []
     file_paths = FS.list_paths_in(path_to.records)
@@ -19,25 +41,11 @@ def get_recorder_data(*names, quiet=False):
                     for each_path in file_paths
                         if each_name_to_include.lower() in FS.basename(each_path).lower() and each_path not in which_experiments
             ]
-            
-        
+    
     output = []
     for path in which_experiments:
         if not quiet:
             print(f"    parsing: {path}")
-        recorder_path = path+"/recorder.yaml"
-        if FS.is_file(recorder_path):
-            data = LazyDict(
-                ez_yaml.to_object(
-                    string=FS.read(recorder_path)
-                )
-            )
-            data.records = [ LazyDict(each) for each in data.records ]
-            output.append(
-                (
-                    FS.basename(path),
-                    data,
-                )
-            )
+        data = load_recorder(path+"/recorder.yaml")
     
     return output
