d12af4487aeb8a80545dcfacfba1b61b5d689185
Jeff Hykin <jeff.hykin@gmail.com>
2023-05-18 12:25:52 -0500

    move DeterministicTorchRng to trivial_torch_tools

--- main/policies/retrained.py
+++ main/policies/retrained.py
@@ -4,18 +4,18 @@ from random import random, sample, choices
 import torch
 from blissful_basics import FS, print, stringify
 
+from __dependencies__.super_hash import super_hash
+from trivial_torch_tools.misc import DeterministicTorchRng
 from config import config, path_to, absolute_path_to
 from specific_tools.train_ppo import PolicyNetworkGauss, device
 
 pi = torch.load(path_to.saved_policies+ "/manaul_ppo_10000000.pt")
 
-hidden_state = None
-
 def policy(observation):
     with print.indent:
-        import json
         observation = torch.as_tensor(observation, dtype=torch.float32).to(device)
         distribution = pi(observation)
+        with DeterministicTorchRng(observation):
+            action = distribution.sample()
         
-        action = distribution.sample()
         return action.cpu().numpy()
