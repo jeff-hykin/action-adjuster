6db7f13bed456b93a54a70c4e4b7fcd1c287ca2d
Jeff Hykin <jeff.hykin@gmail.com>
2023-05-18 13:28:54 -0500

    add global debug var

--- main/config.py
+++ main/config.py
@@ -1,5 +1,5 @@
 from __dependencies__.quik_config import find_and_load
-from __dependencies__.blissful_basics import FS
+from __dependencies__.blissful_basics import FS, LazyDict
 
 info = find_and_load(
     "main/config.yaml",
@@ -12,6 +12,7 @@ absolute_path_to  = info.absolute_path_to
 path_to           = info.path_to
 config            = info.config
 selected_profiles = list(info.selected_profiles)
+debug = LazyDict()
 
 config.output_folder = config.get("output_folder", path_to.default_output_folder)
 # stamp some things
