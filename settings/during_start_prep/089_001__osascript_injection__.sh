5d7cd83f226cddfc769f3edd4189ef4000d1e7a1
Jeff Hykin <jeff.hykin@gmail.com>
2023-07-25 16:10:22 -0500

    -

--- settings/during_start_prep/089_001__osascript_injection__.sh
+++ settings/during_start_prep/089_001__osascript_injection__.sh
@@ -1,1 +1,4 @@
-"$FORNIX_FOLDER/settings/extensions/#standard/commands/tools/fornix/inject_into_path" "osascript"
+if [ "$(uname)" = "Darwin" ] 
+then
+    "$FORNIX_FOLDER/settings/extensions/#standard/commands/tools/fornix/inject_into_path" "osascript"
+fi
