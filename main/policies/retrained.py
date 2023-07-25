f1a2f47ae236c0ce4efa9cffd9c953834eb64177
Jeff Hykin <jeff.hykin@gmail.com>
2023-07-25 12:02:18 -0500

    running experiment 12

--- main/policies/retrained.py
+++ main/policies/retrained.py
@@ -11,10 +11,14 @@ from config import config, path_to, absolute_path_to, debug
 from specific_tools.train_ppo import PolicyNetworkGauss, device
 
 pi = torch.load(path_to.saved_policies+ "/manaul_ppo_10000000.pt")
+deterministic = True
 
 def policy(observation):
     with print.indent:
         observation = torch.as_tensor(observation.to_numpy(), dtype=torch.float32).to(device)
         distribution = pi(observation)
-        action = distribution.sample()
-        return action.cpu().numpy()
+        if deterministic:
+            action = distribution.loc
+        else:
+            action = distribution.sample()
+        return action.cpu().detach().numpy()
