5ab417cd1418eb8b785a9c6f98b542d54264fcf6
Jeff Hykin <jeff.hykin@gmail.com>
2023-08-15 19:27:04 -0500

    setup experiment 18

--- main/specific_tools/comparision_runs.py
+++ main/specific_tools/comparision_runs.py
@@ -5,7 +5,7 @@ from config import path_to
 from subprocess import Popen, PIPE
 
 
-experiment_number = 17
+experiment_number = 18
 if __name__ == "__main__":
     finished = 0
     number_of_episode_runs_per_scenario = 15
