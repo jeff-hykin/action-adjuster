581a816c51184ebe4f0b2b1ca357a9756640ed59
Jeff Hykin <jeff.hykin@gmail.com>
2023-08-16 17:09:19 -0500

    add notifier

--- main/specific_tools/comparision_runs.py
+++ main/specific_tools/comparision_runs.py
@@ -3,28 +3,41 @@ import subprocess
 import json
 from config import path_to, config
 from subprocess import Popen, PIPE
+from knockknock import telegram_sender
+from __dependencies__.blissful_basics import FS
 
-
-experiment_number = config.experiment_number
-number_of_episode_runs_per_scenario = config.number_of_episode_runs_per_scenario
-if __name__ == "__main__":
-    finished = 0
-    for run_number in range(finished, number_of_episode_runs_per_scenario):
-        processes = []
-        for each_profile in [ "@NORMAL_ADJUSTER", "@NO_ADJUSTER", "@PERFECT_ADJUSTER", ]:
-            run_number_string = f"{run_number}".rjust(3,' ')
-            output_folder = f"{path_to.records}/{experiment_number}.{each_profile}|{run_number_string}.ignore"
-            profiles = [ each_profile, "@WARTHOG", "@NOISE=MEDIUM", "@ADVERSITY=STRONG", "@BATTERY_DRAIN" ]
-            print(f"working on: {output_folder}")
-            process = Popen([ sys.executable, path_to.main, *profiles, f"output_folder:{json.dumps(output_folder)}" ])
-            processes.append(process)
+def notify_if_possible(func):
+    api_token = FS.read(f"{FS.home}/.ssh/default_telegram_bot_token")
+    if api_token:
+        return telegram_sender(token=api_token, chat_id=688903965)(func)
+    else:
+        return func
         
-        for each_process in processes:
-            each_process.wait()
-        # for some reason the processes don't seem to stop even after being waited on
-        for each_process in processes:
-            try: each_process.kill()
-            except: pass
-        for each_process in processes:
-            try: each_process.terminate()
-            except: pass
+
+@notify_if_possible
+def run():
+    experiment_number = config.experiment_number
+    number_of_episode_runs_per_scenario = config.number_of_episode_runs_per_scenario
+    if __name__ == "__main__":
+        finished = 0
+        for run_number in range(finished, number_of_episode_runs_per_scenario):
+            processes = []
+            for each_profile in [ "@NORMAL_ADJUSTER", "@NO_ADJUSTER", "@PERFECT_ADJUSTER", ]:
+                run_number_string = f"{run_number}".rjust(3,' ')
+                output_folder = f"{path_to.records}/{experiment_number}.{each_profile}|{run_number_string}.ignore"
+                profiles = [ each_profile, "@WARTHOG", "@NOISE=MEDIUM", "@ADVERSITY=STRONG", "@BATTERY_DRAIN" ]
+                print(f"working on: {output_folder}")
+                process = Popen([ sys.executable, path_to.main, *profiles, f"output_folder:{json.dumps(output_folder)}" ])
+                processes.append(process)
+            
+            for each_process in processes:
+                each_process.wait()
+            # for some reason the processes don't seem to stop even after being waited on
+            for each_process in processes:
+                try: each_process.kill()
+                except: pass
+            for each_process in processes:
+                try: each_process.terminate()
+                except: pass
+
+run()
