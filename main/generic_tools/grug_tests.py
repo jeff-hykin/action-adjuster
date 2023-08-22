7a51b606a6167691a8d57d49c8e3cf99229d3c79
Jeff Hykin <jeff.hykin@gmail.com>
2023-08-22 11:36:50 -0500

    get alpha grug tests working

--- main/generic_tools/grug_tests.py
+++ main/generic_tools/grug_tests.py
@@ -1,46 +1,57 @@
 from super_hash import super_hash
 from warnings import warn
 import ez_yaml
+from blissful_basics import FS, large_pickle_save, large_pickle_load
 
+# TODO:
+    # add additional inputs
+    # add file path arg inputs
+    # add CLI for replay mode
 
 def path_of_caller(*paths):
     import os
     import inspect
     
-    cwd = os.getcwd()
+    intial_cwd = os.getcwd()
     # https://stackoverflow.com/questions/28021472/get-relative-path-of-caller-in-python
     try:
         frame = inspect.stack()[2]
         module = inspect.getmodule(frame[0])
-        directory = os.path.dirname(module.__file__)
+        directory = module.__file__
     # if inside a repl (error =>) assume that the working directory is the path
     except (AttributeError, IndexError) as error:
         directory = cwd
     
-    if is_absolute_path(directory):
-        return join(directory, *paths)
+    if FS.is_absolute_path(directory):
+        return FS.join(directory, *paths)
     else:
         # See note at the top
-        return join(intial_cwd, directory, *paths)
+        return FS.join(intial_cwd, directory, *paths)
 
 class GrugTest:
     production_override = False
     replay_inputs = False
-    record_outputs = True
-    record_inputs = False
+    record_io = True
     verbose = False
-    project_folder = "./tests/grug_tests/" # FIXME: walk up until .git
+    project_folder = "./" # FIXME: walk up until .git
     test_folder = "./tests/grug_tests/" # FIXME: walk up until .git
 
 test_counts = {}
 def grug_test():
+    """
+    @grug_test()
+    def pure_function(arg1, arg2):
+        return arg1 + arg2
+    
+    GrugTest.record_io
+    """
     def decorator(function_being_wrapped):
         if GrugTest.production_override:
             # no wrapping
             return function_being_wrapped
-        if GrugTest.record_inputs:
+        if GrugTest.record_io:
             source = path_of_caller()
-            relative_path = GrugTest.test_folder+"/"+FS.make_relative_path(coming_from=source, to=GrugTest.project_folder)
+            relative_path = GrugTest.test_folder+"/"+FS.make_relative_path(coming_from=GrugTest.project_folder, to=source)
             function_name = getattr(function_being_wrapped, "__name__", "<unknown_func>")
             grug_folder_for_this_func = relative_path+"/"+function_name
             FS.ensure_is_folder(grug_folder_for_this_func)
@@ -48,26 +59,33 @@ def grug_test():
         class YamlFailed: pass
         
         def wrapper(*args, **kwargs):
-            grug_is_testing = GrugTest.record_inputs or GrugTest.record_outputs
-            if grug_is_testing:
+            grug_is_recording = GrugTest.record_io
+            if grug_is_recording:
                 try:
                     arg = (args, kwargs)
-                    input_hash = super_hash(arg)
+                    input_hash = super_hash(arg)[0:12] # 12 chars is plenty for being unique 
                 except Exception as error:
                     warn(f"\n\n\nFor a grug test on this function: {repr(function_name)} I tried to hash the inputs but I wasn't able to.\nHere are the input types:\n    args: {repr(tuple(type(each) for each in args))}\n    kwargs: {repr(tuple(type(each) for each in kwargs.values()))}\nAnd here's the error: {error}", category=None, stacklevel=1, source=source)
-                    
-            if GrugTest.record_inputs:
+                
                 try:
                     yaml_path = grug_folder_for_this_func+f"/{input_hash}.input.yaml"
                     pickle_path = grug_folder_for_this_func+f"/{input_hash}.input.pickle"
-                    if not (FS.exists(yaml_path) or FS.exits(pickle_path)):
+                    print(f'''input_hash = {input_hash}''')
+                    print(f'''FS.exists(yaml_path) = {FS.exists(yaml_path)}''')
+                    print(f'''FS.exists({pickle_path}) = {FS.exists(pickle_path)}''')
+                    if not (FS.exists(yaml_path) or FS.exists(pickle_path)):
                         yamlized_function_input = YamlFailed
                         try:
-                            yamlized_function_input = ez_yaml.to_obj(arg)
+                            yamlized_function_input = ez_yaml.to_string(obj=dict(args=args,kwargs=kwargs))
                             FS.write(data=yamlized_function_input, to=yaml_path)
+                            print(f'''    wrote input to: {yaml_path}''')
                         except Exception as error:
-                            large_pickle_save((args, kwargs), pickle_path)
+                            raise error
+                            print(f'''error = {error}''')
+                            # large_pickle_save((args, kwargs), pickle_path)
+                            print(f'''    wrote input to: {pickle_path}''')
                 except Exception as error:
+                    raise error
                     warn(f"\n\n\nFor a grug test on this function: {repr(function_name)} I tried to seralize the inputs but I wasn't able to.\nHere are the input types:\n    args: {repr(tuple(type(each) for each in args))}\n    kwargs: {repr(tuple(type(each) for each in kwargs.values()))}\nAnd here's the error: {error}", category=None, stacklevel=1, source=source)
             
             the_error = None
@@ -76,12 +94,12 @@ def grug_test():
             except Exception as error:
                 the_error = error
                 
-            if GrugTest.record_outputs:
+            if grug_is_recording:
                 yaml_path = grug_folder_for_this_func+f"/{input_hash}.output.yaml"
                 pickle_path = grug_folder_for_this_func+f"/{input_hash}.output.pickle"
                 grug_output = { "error_output": repr(the_error), "normal_output": output }
                 try:
-                    yamlized_function_output = ez_yaml.to_obj(grug_output)
+                    yamlized_function_output = ez_yaml.to_string(obj=grug_output)
                     FS.write(data=yamlized_function_output, to=yaml_path)
                 except Exception as error:
                     try:
@@ -95,3 +113,14 @@ def grug_test():
             return output
         return wrapper
     return decorator
+
+
+@grug_test()
+def add_nums(a,b):
+    return a + b
+
+
+GrugTest.record_io = True
+for a,b in zip(range(10), range(30, 40)):
+    add_nums(a,b)
+
